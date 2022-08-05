1d59ff0f7ee3a023ba8572e96c6a1cd2af9b3245
JesusCampoLopez <80459614+JesusCampoLopez@users.noreply.github.com>
2022-08-05 20:25:41 +0300

    Update descargarEntregas.py

--- descargarEntregas.py
+++ descargarEntregas.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-#
-# Example for retrieving submission details
-#
 
-import argparse
 import sys
 import os
 import pathlib
-import re	#Para eliminar las etiquetas html de un texto
+import re
+import shutil
 
 from pathlib import Path
 from moodleteacher.connection import MoodleConnection      # NOQA
@@ -16,60 +13,71 @@ from moodleteacher.assignments import MoodleAssignment      # NOQA
 from moodleteacher.users import MoodleUser      # NOQA
 from moodleteacher.files import MoodleFile      # NOQA
 
-# Allow execution of script from project root, based on the library
-# source code
+# Permitimos la ejecución de secuencias de comandos desde la raiz del proyecto
+# en función del código fuente
 sys.path.append(os.path.realpath('.'))
 
-# Funcion que sirve para eliminar todas las etiquetas html de un texto introducido para poder rescatar
-# el texto correspondiente a los comentarios de las entregas de los usuarios en una tarea
 def strip_tags(valor):
+	"""
+        Elimina todas las etiquetas html de un texto introducido.
+        """
 	return re.sub(r'<[^>]*?>', '', valor)
 
 
 def descargarEntregas(conn, courseid, assignmentname):
-	#import logging
-	#logging.basicConfig(level=logging.DEBUG)
-
-    	# Prepare connection to your Moodle installation.
-    	# The flag makes sure that the user is asked for credentials, which are then
-    	# stored in ~/.moodleteacher for the next time.
-	#conn = MoodleConnection(interactive=True)
+	"""
+        Descarga todas las entregas realizadas por los usuarios sobre la tarea indicada, creando un directorio para cada usuario
+        el cual contiene todos los ficheros de su entrega.
+        
+        Args:
+            conn:        	El objeto de MoodleConnection.
+            courseid:    	Id del curso de la tarea.
+            assignmentname: 	Nombre de la tarea.
+        Returns:
+            users: 		Lista de los nombres de los usuarios que han realizado la entrega
+            ficheros:		Lista de los nombres de los ficheros que han entregado los usuarios
+	"""
 	
-	#Almaceno la ruta del fichero actual
+	# Almaceno la ruta del fichero actual
 	path = pathlib.Path().absolute()
 
-	#Creamos los objetos de curso y assignment correspondientes en funcion de los argumentos recibidos
+	# Creamos los objetos de curso y assignment correspondientes en funcion de los argumentos recibidos
 	course = MoodleCourse.from_course_id(conn, courseid)
 	assignment = MoodleAssignment.from_assignment_name(course, assignmentname)
 	
-	numAlumnos = 0	#Variable para contabilizar el numero de alumnos que han realizado la entrega para el assignment
 	ficheros = []
 	users = []
 	
-	os.mkdir('Entregas' + '_' + assignment.name)
+	# Definimos y creamos el directorio que contendrá todas las entregas
+	if not os.path.exists('Entregas' + '_' + assignment.name):
+		os.mkdir('Entregas' + '_' + assignment.name)
         
 	for submission in assignment.submissions():
-		#Comprobamos si existe alguna entrega
+		# Comprobamos si existe alguna entrega
 		if (len(submission.files) != 0):
-			numAlumnos += 1
 		
 			user = MoodleUser.from_userid(conn, submission.userid)
-			#Creamos un directorio para almacenar todos los archivos que ha entregado el alumno
 			
 			nombreUsuario = user.fullname.replace(" ","")
 			users.append(user)
 			
+			if os.path.exists('Entregas' + '_' + assignment.name + '/' + assignment.name + '_' + nombreUsuario):
+				shutil.rmtree(str(path) + '/' + 'Entregas' + '_' + assignment.name + '/' + assignment.name + '_' + nombreUsuario)
+			
+			# Creamos un directorio para cada usuario que ha realizado una entrega
+			print("Entro a crear a " + user.fullname)
 			os.makedirs(os.path.join('Entregas' + '_' + assignment.name , assignment.name + '_' + nombreUsuario))
-			#En caso de tener comentario, mostramos el comentario del usuario por pantalla
+			
+			# En caso de tener comentario, mostramos el comentario del usuario por pantalla
 			if submission.textfield:
 				print("Comentarios de " + nombreUsuario + ":\n" + strip_tags(submission.textfield) + "\n")
 			for f in submission.files:
-				#Descargamos o descomprimimos los ficheros entregados del alumno en su carpeta correspondiente
+				# Descargamos o descomprimimos los ficheros entregados del alumno en su carpeta correspondiente
 				f.unpack_to(str(path) + '/' + 'Entregas' + '_' + assignment.name + '/' + 
 					assignment.name + '_' + nombreUsuario + '/', False)
 				ficheros.append(f.name)
 			
-	print("Hay un total de " + str(numAlumnos) + " entregas realizadas de un total de " + str(len(course.users)-1) + " alumnos matriculados")
+	print("Hay un total de " + str(len(users)) + " entregas realizadas de un total de " + str(len(course.users)-1) + " alumnos matriculados")
 	print("\nLos ficheros han sido descargados correctamente")
 	
-	return numAlumnos, users, ficheros
+	return users, ficheros
