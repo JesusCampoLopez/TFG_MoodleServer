d23bd5f266549137fc32bc68edc5f240fefc5f9e
JesusCampoLopez <80459614+JesusCampoLopez@users.noreply.github.com>
2022-08-09 20:27:22 +0300

    Update PruebaGeneral.py

--- PruebaGeneral.py
+++ PruebaGeneral.py
@@ -21,16 +21,14 @@ from moodleteacher.requests import MoodleRequest      # NOQA
 from moodleteacher.assignments import MoodleAssignment
 
 if __name__ == '__main__':
-	#import logging
-	#logging.basicConfig(level=logging.DEBUG)
 
-    	# Prepare connection to your Moodle installation.
-    	# The flag makes sure that the user is asked for credentials, which are then
-    	# stored in ~/.moodleteacher for the next time.
+    	# Preparamos la conexion a nuestro Moodle
+    	# Con la flag interactive puesta en True nos aseguramos que el usuario es preguntado por sus credenciales, 
+    	# los cuales se almacenaran en ~/.moodleteacher para la siguiente conexión
 	conn = MoodleConnection(interactive=True)
 
 
-	#Añado los argumentos id del curso y nombre de la tarea
+	# Añado los argumentos id del curso y nombre de la tarea
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-c", "--courseid", help="Course ID (check view.php?id=...).", required=True, type=int)
 	parser.add_argument("-a", "--assignmentname", help="Assignment name (check the name of the assignment).", required=True, type=str)
@@ -44,36 +42,34 @@ if __name__ == '__main__':
 		print("No existe ninguna tarea con ese nombre")
 		exit() 
 	
-	#Primero de todo descargamos todas las entregas realizadas por los alumnos en esta tarea
-	#Recopilamos el numero de alumnos que han hecho entregas, los usuarios en sí y los nombres de los ficheros que entregaron
+	# Primero de todo descargamos todas las entregas realizadas por los alumnos en esta tarea
+	# Recopilamos el numero de alumnos que han hecho entregas, los usuarios en sí y los nombres de los ficheros que entregaron
 	u, f = descargarEntregas.descargarEntregas(conn, course, assignment)
 	
-	#Establecemos el directorio donde se almacenan todas las entregas
+	# Establecemos el directorio donde se almacenan todas las entregas
 	path = "Entregas_" + args.assignmentname
 	
-	#Descargamos la hoja de calificaciones de dicha tarea
+	# Descargamos la hoja de calificaciones de dicha tarea
 	hojacalificaciones.descarga(conn, course, assignment)
 	
-	#Este diccionario contendra la tupla del id del usuario y su respectiva nota en la tarea
+	# Este diccionario contendra la tupla del id del usuario y su respectiva nota en la tarea
 	diccionario = {}
 	
-	#Recorremos todas las entregas realizadas por los alumnos
+	# Recorremos todas las entregas realizadas por los alumnos
 	for i in range(len(u)):
-		#Esta sera la ruta del alumno correspondiente a esta iteracion
+		# Esta sera la ruta del alumno correspondiente a esta iteracion
 		totalpath = path + "/" + args.assignmentname + "_" + u[i].fullname.replace(" ","") + "/" + f[i]
 		
-		#Ejecutamos la herramienta correspondiente para calificar la entrega
+		# Ejecutamos la herramienta correspondiente para calificar la entrega
 		r = subprocess.call(["./intro_sw_static_check.sh",totalpath,"config.yaml"])
 		
-		#Actualizamos el diccionarios, con el par id de usuario y su nota
+		# Actualizamos el diccionarios, con el par id de usuario y su nota
 		diccionario[u[i].id_] = r
 	
 	nombreArchivo = 'Calificaciones_' + args.assignmentname + '.csv'
 	
-	#Una vez calificadas todas las entregas con la herramienta, actualizamos nuestra hoja de calificaciones apuntando las notas	
+	# Una vez calificadas todas las entregas con la herramienta, actualizamos nuestra hoja de calificaciones apuntando las notas	
 	hojacalificaciones.apuntarNotas(diccionario, nombreArchivo)
 	
-	#Una vez actualizadas las notas en la hoja de calificaciones, a partir de la misma subimos las notas a Moodle
+	# Una vez actualizadas las notas en la hoja de calificaciones, a partir de la misma subimos las notas a Moodle
 	hojacalificaciones.subir(conn, assignment)
-	
-	
